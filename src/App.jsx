30c79ee95da826577798ca3958d1272be6792495
Harhat18 <harunhatib18@gmail.com>
2023-11-06 19:54:28 +0300

    add first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Navbar from "../components/navbar/Navbar";
+import "./app.scss";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section>
+        <Navbar />
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portflio1</section>
+      <section>Portflio2</section>
+      <section>Portflio3</section>
+      <section>Portflio4</section>
+    </div>
+  );
 };
 
 export default App;
